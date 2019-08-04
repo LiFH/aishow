d9c5ba6a027cb71465b8720455658a5489439812
LiFH <fh.li@foxmail.com>
2019-08-04 11:41:58 +0800

    修改主页

--- aishowfront/src/pages/index.js
+++ aishowfront/src/pages/index.js
@@ -1,60 +1,63 @@
 import styles from './index.css';
 import { Carousel } from 'antd';
 import { Typography, Divider } from 'antd';
-
+import React from "react";
 const { Title, Paragraph, Text } = Typography;
-export default function() {
-  return (
-    <div>
-<Carousel autoplay>
+export default class Face extends React.Component {
+  render(){
+    return (
       <div>
-      <h3>人脸检测</h3>
-        <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
-      </div>
-      <div>
-        <h3>色情识别</h3>
-        <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
-      </div>
-      <div>
-        <h3>场景识别</h3>
-        <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
-      </div>
-      <div>
-        <h3>图像超分辨率</h3>
-        <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
-      </div>
-  </Carousel>
-
-
-<Typography>
-    <Title>Introduction</Title>
-    <Paragraph>
-      In the process of internal desktop applications development, many different design specs and
-      implementations would be involved, which might cause designers and developers difficulties and
-      duplication and reduce the efficiency of development.
-    </Paragraph>
-    <Paragraph>
-      After massive project practice and summaries, Ant Design, a design language for background
-      applications, is refined by Ant UED Team, which aims to{' '}
-      <Text strong>
-        uniform the user interface specs for internal background projects, lower the unnecessary
-        cost of design differences and implementation and liberate the resources of design and
-        front-end development
-      </Text>
-      .
-    </Paragraph>
-    <Title level={2}>Guidelines and Resources</Title>
-    <Paragraph>
-      We supply a series of design principles, practical patterns and high quality design resources
-      (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
-      prototypes beautifully and efficiently.
-    </Paragraph>
-
+  <Carousel autoplay>
+        <div>
+        <h3>人脸检测</h3>
+          <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
+        </div>
+        <div>
+          <h3>色情识别</h3>
+          <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
+        </div>
+        <div>
+          <h3>场景识别</h3>
+          <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
+        </div>
+        <div>
+          <h3>图像超分辨率</h3>
+          <img src={require('../image/1.jpg')} style={{height:'500px'}}/>
+        </div>
+    </Carousel>
   
-    <Divider />
-
-  </Typography>
-    </div>
+{/*   
+  <Typography>
+      <Title>Introduction</Title>
+      <Paragraph>
+        In the process of internal desktop applications development, many different design specs and
+        implementations would be involved, which might cause designers and developers difficulties and
+        duplication and reduce the efficiency of development.
+      </Paragraph>
+      <Paragraph>
+        After massive project practice and summaries, Ant Design, a design language for background
+        applications, is refined by Ant UED Team, which aims to{' '}
+        <Text strong>
+          uniform the user interface specs for internal background projects, lower the unnecessary
+          cost of design differences and implementation and liberate the resources of design and
+          front-end development
+        </Text>
+        .
+      </Paragraph>
+      <Title level={2}>Guidelines and Resources</Title>
+      <Paragraph>
+        We supply a series of design principles, practical patterns and high quality design resources
+        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
+        prototypes beautifully and efficiently.
+      </Paragraph>
+  
     
-  );
+      <Divider />
+  
+    </Typography> */}
+      </div>
+      
+    );
+  }
+
 }
